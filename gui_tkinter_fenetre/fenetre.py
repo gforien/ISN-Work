204db49f7efd9110cd79a535b6302b24e198335b
Kyotosworld <kyoto.public@hotmail.fr>
2014-12-08 22:14:57 +0100

    interface graphique basique utilisant tkinter ajoutée gui_tkinter_fenetre

--- gui_tkinter_fenetre/fenetre.py
+++ gui_tkinter_fenetre/fenetre.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import tkinter as t
+
+class Fenetre(t.Tk):
+    def __init__(self):
+        t.Tk.__init__(self)
+
+        self.label = t.Label(self, text="Hello World !")
+        self.label.pack()
+
+        self.bouton = t.Button(self, text="Quitter", command=self.quit)
+        self.bouton.pack()
+
+fen = Fenetre()
+fen.mainloop()
